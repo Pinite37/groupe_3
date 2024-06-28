70c58fc4399d2c2be5e8f5d6509873db853ea5c5
AGBOSSOUNON-Mariane <136511999+AGBOSSOUNON-Mariane@users.noreply.github.com>
2024-06-28 21:48:09 +0100

    modification de app.py

--- task_3/app.py
+++ task_3/app.py
@@ -3,10 +3,20 @@ from tkinter.ttk import Progressbar
 from PIL import Image, ImageTk
 import time
 import threading
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/hf-internal-testing/tiny-stable-diffusion-pipe"
+head = {"Authorization": "Bearer hf_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"}
+
+# Function to generate image
+def query(payload):
+	response = requests.post(API_URL, headers=head, json=payload)
+	return response.content
 
 # Function to simulate image generation
 def generate_image():
     threading.Thread(target=simulate_loading).start()
+    
 
 def simulate_loading():
     spinner.grid(row=1, column=1, padx=10, pady=10)
@@ -20,16 +30,16 @@ def display_image():
     spinner.stop()  # Stop the spinner
     spinner.grid_remove()  # Hide the spinner
     # Load an example image (replace with actual image generation)
-    img = Image.open("task_3/test.jpg")  # Ensure this image is in the same directory
+    img = Image.open(image_bytes)  # Ensure this image is in the same directory
     img = img.resize((500, 300), Image.LANCZOS)
     img = ImageTk.PhotoImage(img)
     image_label.config(image=img)
-    image_label.image = img
+    image_label['image'] = img
 
 # Create main window
 Win = Tk()
 Win.title("Visualizer")
-Win.geometry("1100x800")
+Win.geometry("1100x800") 
 Win.minsize(1000, 800)
 Win.config(background="#FFFFFC")
 
@@ -43,8 +53,12 @@ info1.pack()
 
 # Description input
 descr = StringVar()
+descript=str(descr)
 entree = Entry(Win, textvariable=descr, bg="#d6cdcd", font=("Arial", 17))
 entree.pack(fill="x", padx=200, pady=20)
+
+# Image generating
+image_bytes = query({"inputs": descript,})
 
 # Button to generate image
 crea = Button(Win, text="Generate Image", font=("Arial", 20), bg="#d6cdcd", command=generate_image)
